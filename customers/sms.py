9b729b13b80fb903204feeaec3c4f450c67550a2
Roney <roney@NDK36-roney-mac.local>
2025-01-28 12:21:39 +0300

    SMS Natification

--- customers/sms.py
+++ customers/sms.py
@@ -1,13 +1,14 @@
 import africastalking
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Order
 from django.conf import settings
 
 africastalking.initialize(settings.AFRICASTALKING_USERNAME, settings.AFRICASTALKING_API_KEY)
-
 sms = africastalking.SMS
 
-def send_sms(phone_number, message):
-    try:
-        response = sms.send(message, [phone_number])
-        return response
-    except Exception as e:
-        print(f"SMS failed: {str(e)}")
+@receiver(post_save, sender=Order)
+def send_sms_on_order_creation(sender, instance, created, **kwargs):
+    if created:
+        message = f"Hello {instance.customer.name}, your order for {instance.item} has been received."
+        sms.send(message, [instance.customer.phone_number])
--- savannah_project/settings.py
+++ savannah_project/settings.py
@@ -167,3 +167,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
